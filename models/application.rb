4ce076d260589dd7575509a3c49599a39d520e5c
Jelle Helsen <jelle.helsen@hcode.be>
2013-10-03 19:08:37 +0200

    initial commit

---
+++ models/application.rb
@@ -0,0 +1,18 @@
+class Application
+  include Mongoid::Document
+  include Mongoid::Timestamps # adds created_at and updated_at fields
+
+  # field <name>, :type => <type>, :default => <value>
+  field :name, :type => String
+  field :key, :type => String
+  before_create :generate_key
+
+  # You can define indexes on documents using the index macro:
+  # index :field <, :unique => true>
+
+  # You can create a composite key in mongoid to replace the default id using the key macro:
+  # key :field <, :another_field, :one_more ....>
+  def generate_key
+    self.key = SecureRandom.hex
+  end
+end
